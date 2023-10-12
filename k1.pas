d9c782a80097038773438c8ccaca2cc177343626
Ownosh <144677630+Ownosh@users.noreply.github.com>
2023-10-12 12:13:38 +0300

    Add files via upload

---
+++ k1.pas
@@ -0,0 +1,14 @@
+﻿program k1;
+var
+  x,r:real;
+begin
+  Write('Напишите x=');
+  read(x);
+  if (x < -8) then
+    r:=cos(x)-x
+  else if (-8 <= x) and (x < -3) then
+    r:=(x*x*x)
+  else
+    r:= cos(x)/Power(x,0.1 * x)+Power(x,0.1 * x);
+  writeln('Значение:',r:2:2)
+end.
---
+++ k2.pas
@@ -0,0 +1,19 @@
+﻿var
+  x, r: real;
+begin
+x:= -10;
+
+while x <= -1 do 
+begin
+  if (x < -8) then
+    r:=cos(x)-x
+  else if (-8 <= x) and (x < -3) then
+    r:=(x*x*x)
+  else
+    r:= cos(x)/Power(x,0.1 * x)+Power(x,0.1 * x);
+  writeln ('Значение:', r:2:2);
+  x+=0.1;
+end;
+end.
+
+
